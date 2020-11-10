bbde94cb98152ed35556a1610e7253e78bcc921d
Natalya POida <nataly.poida@gmail.com>
2020-11-10 21:25:25 +0300

    feat: add default poster, combine components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import Search from './components/Search';
 
-function App() {
+const MOVIE_API_URL = 'https://www.omdbapi.com/?s=green&apikey=83c8c692'
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      setMovies(jsonResponse.Search);
+      setLoading(false);
+    });
+  }, [])
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=83c8c692`)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      if(jsonResponse.Response === 'True') {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      } else {
+        setErrorMessage(jsonResponse.Error);
+        setLoading(false);
+      }
+    });
+  };
   return (
-    <div> Hello </div>
+    <div className="App">  
+    <Header text="MOVIE SEARCH WITH REACT HOOKS" />
+    <Search search={search} />
+    <p className="App-intro">Let's see what we've found!</p>
+    <div className="movies">
+      {loading && !errorMessage ? (
+        <span>loading...</span>
+      ) : errorMessage ? (
+        <div className="errorMessage">{errorMessage}</div>
+      ): (
+        movies.map((movie, index) => (
+<Movie key={`${index}-${movie.Title}`} movie={movie}
+/>        ))
+      )
+      }}
+    </div>
+    </div>
+    
   )
 }
 
